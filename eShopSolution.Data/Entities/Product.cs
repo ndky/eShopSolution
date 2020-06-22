b0282cc7dc3c835287bf7eb05193edbf6f4b7cbb
ky nguyen dinh <ky.nguyendinh@outlook.com>
2020-06-22 15:16:34 +0700

    Lesson 8: Configure entity with fluent API

--- eShopSolution.Data/Entities/Product.cs
+++ eShopSolution.Data/Entities/Product.cs
@@ -6,7 +6,6 @@ using System.Text;
 
 namespace eShopSolution.Data.Entities
 {
-    [Table("Products")]
     public class Product
     {
         public int Id { get; set; }
@@ -15,7 +14,13 @@ namespace eShopSolution.Data.Entities
         public int Stock { get; set; }
         public int ViewCount { get; set; }
         public DateTime DateCreated { get; set; }
-        [Required]
         public string SeoAlias { get; set; }
+        public List<ProductInCategory> ProductInCategories { get; set; }
+
+        public List<OrderDetail> OrderDetails { get; set; }
+
+        public List<Cart> Carts { get; set; }
+
+        public List<ProductTranslation> ProductTranslations { get; set; }
     }
 }
