f7d48aaae1c78fcc88d50d06c74c9132646a8010
ky nguyen dinh <ky.nguyendinh@outlook.com>
2020-06-23 10:26:42 +0700

    lesson 9: commit migration database

--- eShopSolution.Data/EF/EShopDbContextFactory.cs
+++ eShopSolution.Data/EF/EShopDbContextFactory.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Text;
 
 namespace eShopSolution.Data.EF
 {
@@ -13,9 +10,9 @@ namespace eShopSolution.Data.EF
         public EShopDbContext CreateDbContext(string[] args)
         {
             IConfigurationRoot configuration = new ConfigurationBuilder()
-               .SetBasePath(Directory.GetCurrentDirectory())
-               .AddJsonFile("appsettings.json")
-               .Build();
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
 
             var connectionString = configuration.GetConnectionString("eShopSolutionDb");
 
